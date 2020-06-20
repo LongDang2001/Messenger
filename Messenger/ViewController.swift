289be04c38d94bcf8db99280ffa48d9d76d8288e
LongDang2001 <62790457+LongDang2001@users.noreply.github.com>
2020-06-20 22:12:22 +0700

    Gestting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by admin on 6/20/20.
+//  Copyright © 2020 Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
